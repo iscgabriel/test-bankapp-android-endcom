f6d5ea78fc9ad6d4549a892be7995d52e2c5e0cc
Gabriel Valles de la Torre <iscgabrielvalles@gmail.com>
2022-07-25 20:54:19 -0500

    Commit inicial

---
+++ app/src/main/java/com/gabrielvalles/test_bankapp_android_endcom/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gabrielvalles.test_bankapp_android_endcom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
